c660b9e0c237057d0524730fdcac5233c2fde8b3
FunEXP <famxinlong@gmail.com>
2022-01-04 11:49:46 +0000

    Learnt Svelte CSS File

---
+++ FrontEnd.md
@@ -0,0 +1,8 @@
+# HTML
+- forms because of actions that needs to be submitted to the backend
+- ctrl-d to select all occurences and replace respectively
+- `$` to point to current directory (svelte specific)
+
+# CSS
+- `__layout.svelte` contains code that applies to all the files and diretories in the current directory
+    - the content of the page also needs to be dynamically changed with `slot`
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,18 +1,33 @@
 <script>
-    import TodoItem from "../lib/todo-item.svelte";
+    import TodoItem from "$lib/todo-item.svelte";
 
     const title="Todo";
 </script>
+
+<!-- To target css styles on this component specifically -->
+<style>
+    .todos {
+        width: 100%;
+        max-width: 42rem;
+        margin: 4rem auto 0 auto;
+    }
+
+    .new{
+
+    }
+</style>
 
 <!-- App Name that changes base on the page-->
 <svelte:head>
     <title>{title}</title>
 </svelte:head>
 
-<h1>{title}</h1>
+<div class="todos">
+    <h1>{title}</h1>
 
-<form action="" method="">
-    <input type="text" name="text" aria-label="Add a todo" placeholder="+ tap to add a todo">
-</form>
+    <form action="" method="">
+        <input type="text" name="text" aria-label="Add a todo" placeholder="+ tap to add a todo">
+    </form>
 
-<TodoItem/>
+    <TodoItem/>
+</div>
